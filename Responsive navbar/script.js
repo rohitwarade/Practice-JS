7c8bbcddd4376cbfd83ad50afb14db897d73a388
rohitwarade <waraderp@gmail.com>
2023-05-24 23:04:16 +0530

    initial commit

--- Responsive navbar/script.js
+++
@@ -1,6 +0,0 @@
-const menuw = document.querySelector(".fa-solid");
-const navlink = document.querySelector(".nav-bar");
-
-menuw.addEventListener('click',()=>{
-    navlink.classList.toggle('mobile-menu')
-})
